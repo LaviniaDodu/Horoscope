6bffd0a21d250c3702553b2172624b6e8f366629
LaviniaDodu <82836346+LaviniaDodu@users.noreply.github.com>
2023-01-21 18:55:09 +0100

    Installed dependencies, bootstrap, maatwebsite/excel Package, migrations

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: [...refreshPaths,],
+        }),
+    ],
+});
